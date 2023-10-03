ded679a41831d5507a0243e9ee84b216e9f4ae9d
Vrunda Joshi <vrundajoshi@Vrundas-Air.mshome.net>
2023-10-03 18:41:39 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './style.css';
 
-function App() {
+const ROWS = 8;
+const COLS = 8;
+
+const ClickableGrid = () => {
+  const [isRedCell, setIsRedCell] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid-container'>
+      {Array(ROWS)
+        .fill(false)
+        .map((_, rowIndex) =>
+          Array(COLS)
+            .fill(false)
+            .map((_, colIndex) => (
+              <div
+                key={`${rowIndex}_${colIndex}`}
+                className={`grid-item ${
+                  (rowIndex + colIndex) % 2 === 0 ? 'black' : ''
+                } ${
+                  isRedCell.includes(`${rowIndex}_${colIndex}`) ? 'red' : ''
+                }`}
+                onClick={() => setIsRedCell(`${rowIndex}_${colIndex}`)}
+              ></div>
+            ))
+        )}
     </div>
   );
-}
+};
 
-export default App;
+export default ClickableGrid;
